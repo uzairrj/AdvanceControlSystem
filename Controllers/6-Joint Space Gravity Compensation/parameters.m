bb4191085ef8c1f1588cfeefd620ea043ddb500d
uzairrj <uzairrj@gmail.com>
2023-01-31 21:25:29 +0100

    Updating gravity copensation controller parameters

--- Controllers/6-Joint Space Gravity Compensation/parameters.m
+++ Controllers/6-Joint Space Gravity Compensation/parameters.m
@@ -2,8 +2,22 @@
 A = 1;
 Fc = 1;
 
+%lengths
+lenghts_values = [[0.02, 0.1256, 0.4]; 
+                  [0.03, 0.03, 0.3]; 
+                  [0.02, 0.1256, 0.16]];
+density = 2700; %Almunium density
+mass_values = [pi * lenghts_values(1,1)^2*lenghts_values(1,3)*density, lenghts_values(2,1)*lenghts_values(2,2)*lenghts_values(3,3)*density, lenghts_values(3,1)^2*lenghts_values(3,3)*density];
+           
+
+%masses
+params.m = mass_values;
+
+%gravity
+params.g = 9.806;
+
 %Joint variables values
-qd = [1.3, -0.5, -1.3];
+qd = [1.3, -0.25, -1.3];
 
 %PD control values
 Kp = [[100,0,0];
