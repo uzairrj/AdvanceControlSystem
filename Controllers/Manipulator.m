8911b1a1fa0621d5b135fada414f272f1f8d894d
uzairrj <uzairrj@gmail.com>
2023-01-31 21:24:56 +0100

    Friction, generic mass and generic gravity

--- Controllers/Manipulator.m
+++ Controllers/Manipulator.m
@@ -1,4 +1,4 @@
-function [sys, x, str, ts] = Manipulator(t,x,u,flag)
+function [sys, x, str, ts] = Manipulator(t,x,u,flag, parameters)
     if flag == 0 %initialization
         x = zeros(6,1);
         str = [];
@@ -17,9 +17,11 @@ function [sys, x, str, ts] = Manipulator(t,x,u,flag)
         q_dot = x(1:3);
         q = x(4:6);
         tau = u(1:3);
-        B = double(inertiaMatrix(q));
-        C = double(corolisMatrix(q,q_dot));
-        G = double(gravityMatrix(q));
+        parameters.q = q;
+        parameters.q_dot = q_dot;
+        B = double(inertiaMatrix(parameters));
+        C = double(corolisMatrix(parameters));
+        G = double(gravityMatrix(parameters));
         
         q_dot_dot = inv(B)*(tau - C*q_dot - G);
 
@@ -30,36 +32,52 @@ function [sys, x, str, ts] = Manipulator(t,x,u,flag)
     end
 end
 
-function B = inertiaMatrix(q)
-    q1 = q(1); % not used in this matrix
-    q2 = q(2);
-    q3 = q(3);
+function B = inertiaMatrix(parameters)
+    q1 = parameters.q(1); % not used in this matrix
+    q2 = parameters.q(2);
+    q3 = parameters.q(3);
 
-    B = [[(1377*q2)/6250 + (5286372695289*pi)/76293945312500 + (864*cos(q3))/78125 + (864*cos(q3)^2)/390625 + (351*q2^2)/625 + 782036240702373/6103515625000000, (216*cos(q3))/15625 + 702/3125, (216*sin(q3)*(q2 + 3/10))/15625]
-        [                                                                                                                       (216*cos(q3))/15625 + 702/3125,                        351/625,                               0]
-        [                                                                                                                      (216*sin(q3)*(q2 + 3/10))/15625,                              0,                881199/244140625]];
+    m1 = parameters.m(1);
+    m2 = parameters.m(2);
+    m3 = parameters.m(3);
+
+    B = [[(195791581307*m1)/1220703125000 + (8503*m2)/40000 + (321279862557*m3)/1220703125000 + (8*m3*cos(q3)^2)/625 + (3*m2*q2)/10 + (3*m3*q2)/5 + m2*q2^2 + m3*q2^2 + (8*m3*cos(q3))/125, (2*m2)/5 + (2*m3)/5 + (2*m3*cos(q3))/25, (2*m3*sin(q3)*(q2 + 3/10))/25]
+        [                                                                                                                                         (2*m2)/5 + (2*m3)/5 + (2*m3*cos(q3))/25,                                 m2 + m3,                             0]
+        [                                                                                                                                                   (2*m3*sin(q3)*(q2 + 3/10))/25,                                       0,            (32637*m3)/1562500]];
 end
 
-function C = corolisMatrix(q, q_dot)
-    q1 = q(1);
-    q2 = q(2);
-    q3 = q(3);
+function C = corolisMatrix(parameters)
+    q1 = parameters.q(1); %not used in this matrix
+    q2 = parameters.q(2);
+    q3 = parameters.q(3);
 
-    q1_dot = q_dot(1);
-    q2_dot = q_dot(2);
-    q3_dot = q_dot(3);
+    q1_dot = parameters.q_dot(1);
+    q2_dot = parameters.q_dot(2);
+    q3_dot = parameters.q_dot(3);
 
-    C = [[q2_dot*((351*q2)/625 + 1377/12500) - q3_dot*((432*sin(2*q3))/390625 + (432*sin(q3))/78125), q1_dot*((351*q2)/625 + 1377/12500), (216*q3_dot*cos(q3)*(q2 + 3/10))/15625 - q1_dot*((432*sin(q3))/78125 + (864*cos(q3)*sin(q3))/390625)]
-        [                         -q1_dot*((351*q2)/625 + 1377/12500) - (216*q3_dot*sin(q3))/15625,                                  0,                                                                            -(216*q1_dot*sin(q3))/15625]
-        [        q1_dot*((432*sin(2*q3))/390625 + (432*sin(q3))/78125) + (216*q2_dot*sin(q3))/15625,         (216*q1_dot*sin(q3))/15625,                                                                                                    0]];
+    m1 = parameters.m(1); %not used in this matrix
+    m2 = parameters.m(2);
+    m3 = parameters.m(3);
+
+
+    C = [[q2_dot*((3*m2)/20 + (3*m3)/10 + m2*q2 + m3*q2) - q3_dot*((4*m3*sin(2*q3))/625 + (4*m3*sin(q3))/125), q1_dot*((3*m2)/20 + (3*m3)/10 + m2*q2 + m3*q2), (2*m3*q3_dot*cos(q3)*(q2 + 3/10))/25 - q1_dot*((4*m3*sin(q3))/125 + (8*m3*cos(q3)*sin(q3))/625)]
+        [                        -q1_dot*((3*m2)/20 + (3*m3)/10 + m2*q2 + m3*q2) - (2*m3*q3_dot*sin(q3))/25,                                              0,                                                                       -(2*m3*q1_dot*sin(q3))/25]
+        [                      q1_dot*((4*m3*sin(2*q3))/625 + (4*m3*sin(q3))/125) + (2*m3*q2_dot*sin(q3))/25,                       (2*m3*q1_dot*sin(q3))/25,                                                                                               0]];
 end
 
-function G = gravityMatrix(q)
-    q1 = q(1);
-    q2 = q(2);
-    q3 = q(3);
-    G = [(1720953*cos(q1))/781250 - (6751431*sin(q1))/6250000 + (264762*cos(q1)*cos(q3))/1953125 + (132381*pi*cos(q1))/156250 - (1720953*q2*sin(q1))/312500;
-                                                                                                                          (1720953*cos(q1))/312500;
-                                                                                                                 -(264762*sin(q1)*sin(q3))/1953125];
+function G = gravityMatrix(parameters)
+    q1 = parameters.q(1);
+    q2 = parameters.q(2);
+    q3 = parameters.q(3);
+    
+    g = parameters.g;
+
+    m1 = parameters.m(1);
+    m2 = parameters.m(2);
+    m3 = parameters.m(3);
+
+    G = [(g*m1*cos(q1))/5 - g*m2*((3*sin(q1))/20 - (2*cos(q1))/5 + q2*sin(q1)) + g*m3*((2*cos(q1))/5 + (2*cos(q1)*cos(q3))/25 - sin(q1)*(q2 + 3/10));
+                                                                                                                        g*cos(q1)*(m2 + m3);
+                                                                                                               -(2*g*m3*sin(q1)*sin(q3))/25];
 end
 
