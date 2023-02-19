85f688288ac8da1775b729d76f06af3443ea1937
uzairrj <uzairrj@gmail.com>
2023-02-19 22:01:32 +0100

    Force control

---
+++ Controllers/14-Force Control/parameters.m
@@ -0,0 +1,41 @@
+%sine parameters
+A = 1;
+Fc = 1;
+
+%lengths
+lenghts_values = [[0.02, 0.1256, 0.4]; 
+                  [0.03, 0.03, 0.3]; 
+                  [0.02, 0.1256, 0.16]];
+density = 2700; %Almunium density
+mass_values = [pi * lenghts_values(1,1)^2*lenghts_values(1,3)*density, lenghts_values(2,1)*lenghts_values(2,2)*lenghts_values(3,3)*density, lenghts_values(3,1)^2*lenghts_values(3,3)*density];
+           
+
+%masses
+params.m = mass_values;
+
+%gravity
+params.g = 9.806;
+
+%Friction
+params.Fv = [0,0,0];
+
+%PD control values
+Kp = diag([40,40,40,1,1,1]);
+Kd = diag([25,25,25,1,1,1]);
+Md = diag([0.12,0.12,0.12,0,0,0]);
+
+%force control parameters
+fd = [0,0,3,0,0,0];
+
+Kf = diag([55,55,55,1,1,1]);
+Ki = diag([50,50,50,1,1,1]);
+
+%enviroment stiffness
+parms.Ke = diag([1,1,2,1,1,1]);
+
+%plane position
+parms.plane_pos = [0,0,0.3,0,0,0]';
+%parms.plane_pos = [0,0,9.0,0,0,0]';
+
+%plane axis
+parms.plane_axis = 3; %z-axis
